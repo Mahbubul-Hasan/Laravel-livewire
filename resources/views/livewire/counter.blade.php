bd738f239cfde2a4678ea214e31b8c0662cc410f
Mahbubul-Hasan <mahbubul161@gmail.com>
2020-11-08 21:45:41 +0600

    Implement: Counter function

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h2>Knowing others is intelligence; knowing yourself is true wisdom.</h2>
+    <button wire:click='increment'> + </button>
+    <span>{{ $number }}</span>
+    <button wire:click='decrement'> - </button>
+</div>
